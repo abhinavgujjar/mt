584bc2ee6b3108c45d2b4ed4ad4fd15d1f96d4a0
Abhinav Gujjar <abhinavgujjar@gmail.com>
2014-09-24 10:40:48 +0530

    Scenarios

--- app/controllers/createController.js
+++ app/controllers/createController.js
@@ -2,13 +2,74 @@ angular.module('holiday')
 	.controller('createController', function($scope, hotelsProvider, $location) {
 
 
+		$scope.hotel = {
+			name: 'default',
+			location: 'Bangalore',
+			price: 10000,
+			ameneties: [{
+				name: 'Swimming Pool',
+				available: true
+			}]
+
+		}
+
+		$scope.states = [{
+			name: 'Karnataka',
+			id: "1"
+		}, {
+			name: 'Tamil Nadu',
+			id: "2"
+		}, {
+			name: 'Kerala',
+			id: "3"
+		}];
+
+
+		var cities = {
+			"1": ['Bangalore', 'Mysore', 'Tumkur'],
+			"2": ['Chennai'],
+			"3": ['Cochin', 'Trissur']
+		};
+
+		$scope.selectedStateId = "2";
+
+
+		$scope.availableCities = cities["3"];
+
+		$scope.$watch('selectedStateId', function(newValue, oldValue) {
+			$scope.availableCities = cities[newValue];
+
+			//default city selection
+			if(cities[newValue].length === 1){
+				$scope.selectedCity = cities[newValue][0];
+			}
+		});
+
+
+		$scope.removeAmenity = function(amenity) {
+
+			var ameneties = $scope.hotel.ameneties;
+			var index = ameneties.indexOf(amenity);
+			ameneties.splice(index, 1);
+
+		}
+
+		$scope.addAmenity = function(newAmenity) {
+			$scope.hotel.ameneties.push({
+				name: newAmenity,
+				available: true
+			});
+
+			$scope.newAmenity = '';
+		}
+
 		$scope.step = 1;
 
-		$scope.incrementStep = function(){
+		$scope.incrementStep = function() {
 			$scope.step++;
 		}
 
-		$scope.decrementStep = function(){
+		$scope.decrementStep = function() {
 			$scope.step--;
 		}
 
